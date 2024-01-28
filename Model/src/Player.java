6fedbb26eed9008b5722d63acafc83237bf17f8a
Ahmad-khatib <ahmad-khatib98@hotmail.com>
2024-01-28 22:52:54 +0200

    First push !

---
+++ Model/src/Player.java
@@ -0,0 +1,30 @@
+public class Player {
+    private String playername;
+    private String color;
+    private int position;
+
+    // Getter and Setter for player name
+    public String getPlayername() {
+        return playername;
+    }
+    public void setPlayername(String playername) {
+        this.playername = playername;
+    }
+
+    // Getter and Setter for player color
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Getter and Setter for player position
+    public int getPosition() {
+        return position;
+    }
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+}
