922044e74d580307a4fdbb606c67ab017ced7634
nooramer2 <nooramer2001@gmail.com>
2024-02-02 19:23:31 +0200

    classes

---
+++ Model/src/Dise.java
@@ -0,0 +1,21 @@
+public class Dise {
+    private int diceId;
+    private int possibleSteps;
+
+    // Getters and Setters for all fields
+    public int getDiceId() {
+        return diceId;
+    }
+
+    public void setDiceId(int diceId) {
+        this.diceId = diceId;
+    }
+
+    public int getPossibleSteps() {
+        return possibleSteps;
+    }
+
+    public void setPossibleSteps(int possibleSteps) {
+        this.possibleSteps = possibleSteps;
+    }
+}
