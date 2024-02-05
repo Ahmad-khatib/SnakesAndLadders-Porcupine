6585a04e4290cd32ea22dc436f964e120be71d07
Ahmad-khatib <ahmad-khatib98@hotmail.com>
2024-02-05 10:30:23 +0200

    ...

---
+++ src/Controller/Start.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("View/MainPage.fxml")));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Model/Snake.java
@@ -0,0 +1,33 @@
+package Model;
+
+public class Snake {
+
+    private int snakeId;
+    private int snakeLength;
+    private String snakeColor;
+
+    // Getters and Setters for all fields
+    public int getSnakeId() {
+        return snakeId;
+    }
+
+    public void setSnakeId(int snakeId) {
+        this.snakeId = snakeId;
+    }
+
+    public int getSnakeLength() {
+        return snakeLength;
+    }
+
+    public void setSnakeLength(int snakeLength) {
+        this.snakeLength = snakeLength;
+    }
+
+    public String getSnakeColor() {
+        return snakeColor;
+    }
+
+    public void setSnakeColor(String snakeColor) {
+        this.snakeColor = snakeColor;
+    }
+}
