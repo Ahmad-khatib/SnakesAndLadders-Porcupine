8e05977baf257ccb766352daf0a22265a876b17d
nooramer2 <nooramer2001@gmail.com>
2024-02-15 17:40:16 +0200

    ...

--- src/Model/Ladder.java
+++ src/Model/Ladder.java
@@ -1,9 +1,18 @@
 package Model;
 
+import java.util.Objects;
 public class Ladder {
     private int ladderId;
     private int ladderLength;
-    private String ladderType;
+    private int startPosition;
+    private int endPosition;
+
+    public Ladder(int ladderId, int ladderLength, int startPosition) {
+        this.ladderId = ladderId;
+        this.ladderLength = ladderLength;
+        this.startPosition = startPosition;
+        this.endPosition = startPosition + ladderLength;
+    }
 
     // Getters and Setters for all fields
     public int getLadderId() {
@@ -22,11 +31,41 @@ public class Ladder {
         this.ladderLength = ladderLength;
     }
 
-    public String getLadderType() {
-        return ladderType;
+    public int getStartPosition() {
+        return startPosition;
     }
 
-    public void setLadderType(String ladderType) {
-        this.ladderType = ladderType;
+    public void setStartPosition(int startPosition) {
+        this.startPosition = startPosition;
     }
+
+    public int getEndPosition() {
+        return endPosition;
+    }
+
+    @Override
+    public String toString() {
+        return "Ladder{" +
+                "ladderId=" + ladderId +
+                ", ladderLength=" + ladderLength +
+                ", startPosition=" + startPosition +
+                ", endPosition=" + endPosition +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ladder ladder = (Ladder) o;
+        return ladderId == ladder.ladderId &&
+                ladderLength == ladder.ladderLength &&
+                startPosition == ladder.startPosition &&
+                endPosition == ladder.endPosition;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ladderId, ladderLength, startPosition, endPosition);
+    }
+
 }
