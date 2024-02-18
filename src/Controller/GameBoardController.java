6125d82425eebd87092a111163a2ebc21529f896
Ahmad-khatib <ahmad-khatib98@hotmail.com>
2024-02-19 01:14:15 +0200

    ...

--- src/Controller/GameBoardController.java
+++ src/Controller/GameBoardController.java
@@ -2,8 +2,10 @@ package Controller;
 
 import Model.GameBoard;
 import Model.Tile;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -11,21 +13,73 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-
+import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class GameBoardController extends GridPane {
 
     @FXML
     private GridPane dynamicGridPane;
+    @FXML
+    private ImageView diceImage;
+    @FXML
+    private ImageView diceImage2;
+    @FXML
+    private Text timerLabel;
+
+
+    @FXML
+    private Button rollButton;
 
     public void initialize(String selectedLevel) throws FileNotFoundException {
         GameBoard gameBoard = new GameBoard(selectedLevel);
         initializeBoardUI(gameBoard);
         placeSnake(selectedLevel);
+        initializeTimer();
+    }
+    Random random = new Random();
+    private void initializeTimer() {
+        AtomicInteger timerSeconds = new AtomicInteger();
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            timerSeconds.getAndIncrement();
+            timerLabel.setText("Timer: " + timerSeconds + " seconds");
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+    @FXML
+    void roll(ActionEvent event) {
+
+        rollButton.setDisable(true);
+
+        Thread thread = new Thread(){
+            public void run(){
+                System.out.println("Thread Running");
+                try {
+                    for (int i = 0; i < 15; i++) {
+                        File file = new File("src/View/photos/dice/dice" + (random.nextInt(5)+1)+".png");
+                        File file2 = new File("src/View/photos/dice/dice" + (random.nextInt(5)+1)+".png");
+                        diceImage.setImage(new Image(file.toURI().toString()));
+                        diceImage2.setImage(new Image(file2.toURI().toString()));
+                        Thread.sleep(50);
+                    }
+                    rollButton.setDisable(false);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        thread.start();
     }
 
-    private void placeSnake(String selectedLevel) {
+    private void placeSnake(String selectedLevel) {          //   NOT READY !!!!
         Image greenSnakeimage = new Image(getClass().getResourceAsStream("/View/Photos/greenSnake.png"));
         Image bludSnakeimage = new Image(getClass().getResourceAsStream("/View/Photos/blueSnake.png"));
         Image yellowSnakeimage = new Image(getClass().getResourceAsStream("/View/Photos/yellowSnake.png"));
@@ -43,7 +97,7 @@ public class GameBoardController extends GridPane {
         greenSnakeimageimageView.setFitWidth(60);
         greenSnakeimageimageView.setVisible(true);
         StackPane stackPane = new StackPane(greenSnakeimageimageView);
-        dynamicGridPane.add(stackPane, 2, 2, 4, 4);
+        dynamicGridPane.add(stackPane, 4, 2, 4, 4);
 
 
         bludSnakeimageimageView.setRotate(60);
@@ -53,7 +107,7 @@ public class GameBoardController extends GridPane {
         bludSnakeimageimageView.setFitWidth(60);
         bludSnakeimageimageView.setVisible(true);
         StackPane bludSnakeimageimageViewstackPane = new StackPane(bludSnakeimageimageView);
-        dynamicGridPane.add(bludSnakeimageimageViewstackPane, 1, 6, 8, 5);
+        dynamicGridPane.add(bludSnakeimageimageViewstackPane, 1, 6, 3, 5);
 
 
         yellowSnakeimageimageView.setRotate(45);
@@ -63,7 +117,7 @@ public class GameBoardController extends GridPane {
         yellowSnakeimageimageView.setFitWidth(30);
         yellowSnakeimageimageView.setVisible(true);
         StackPane yellowSnakeimageimageViewstackPane = new StackPane(yellowSnakeimageimageView);
-        dynamicGridPane.add(yellowSnakeimageimageViewstackPane, 7, 3, 2, 2);
+        dynamicGridPane.add(yellowSnakeimageimageViewstackPane, 2, 3, 2, 2);
 
 
 // Rotate the ImageView 45 degrees (diagonally)
